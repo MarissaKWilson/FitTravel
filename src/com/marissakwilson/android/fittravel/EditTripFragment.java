65043df704176f4de823de6bae66186b5942566c
Marissa K Wilson <Marissa@MarissaKWilson.com>
2015-01-28 09:02:25 -0500

    Added functionality to MeasurementFragment and EditTripFragment

--- src/com/marissakwilson/android/fittravel/EditTripFragment.java
+++ src/com/marissakwilson/android/fittravel/EditTripFragment.java
@@ -1,15 +1,65 @@
 package com.marissakwilson.android.fittravel;
 
+
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class EditTripFragment extends Fragment {
 	
 	private Trip mTrip;
+	private Button mNewStartingButton;
+	private Button mNewEndingButton;
+	private Button mNewTripButton;
+	
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		mTrip = new Trip();
 		}
+	
+	 @Override
+	 public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		 View v = inflater.inflate(R.layout.fragment_edit_trip, parent, false);
+		
+		 mNewStartingButton = (Button)v.findViewById(R.id.button_new_starting);
+		 mNewStartingButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		 mNewEndingButton = (Button)v.findViewById(R.id.button_new_ending);
+		 mNewEndingButton.setOnClickListener(new View.OnClickListener(){
+			 
+			 @Override
+			 public void onClick(View v){
+				 
+				 
+			 }
+			 
+		 });
+		 
+		 mNewTripButton = (Button)v.findViewById(R.id.button_new_trip);
+		 mNewTripButton.setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		 });
+		 
+		 
+		 return v;
+	 }
 
 }
