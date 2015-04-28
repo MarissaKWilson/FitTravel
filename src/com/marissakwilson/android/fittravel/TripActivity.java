676c6aef72a34245d9f1e5e58cacadc24da9e0f9
Marissa K Wilson <Marissa@MarissaKWilson.com>
2015-04-28 09:14:21 -0400

    Fixed R file problem

--- src/com/marissakwilson/android/fittravel/TripActivity.java
+++ src/com/marissakwilson/android/fittravel/TripActivity.java
@@ -43,9 +43,9 @@ public class TripActivity extends FragmentActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
 		return super.onOptionsItemSelected(item);
 	}
 	
